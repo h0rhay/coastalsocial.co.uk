8e55436b802ad9877afe3372bbd32dc06818d8d2
George Clark <clarkgeorge76@gmail.com>
2020-09-20 22:53:13 +0100

    clean out

--- src/utils/withPrefix.js
+++
@@ -1,9 +0,0 @@
-const { withPrefix: gatsbyWithPrefix } = require("gatsby");
-const _ = require('lodash');
-
-export default function withPrefix(url) {
-    if (_.startsWith(url, '#') || _.startsWith(url, 'http://') || _.startsWith(url, 'https://')) {
-        return url;
-    }
-    return gatsbyWithPrefix(url);
-}
