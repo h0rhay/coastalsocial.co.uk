f591615fe28b3dbe75f0857fc30f843c74aebbe5
George Clark <clarkgeorge76@gmail.com>
2020-10-08 14:09:18 +0100

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
             resolve: `gatsby-plugin-google-analytics`,
             options: {
               // The property ID; the tracking code won't be generated without it
-              trackingId: process.env.GA_TRACKING_ID,
+              trackingId: `UA-180103972-1`,
             },
         }
     ]
